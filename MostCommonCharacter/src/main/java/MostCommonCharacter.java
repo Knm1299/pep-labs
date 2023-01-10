0e4caba9e65507aff380fa51eed40e9bf172c7c9
Keven N Mitchell <keven.mitchell@revature.com>
2023-01-10 17:23:00 +0000

    Saved progress on MostCommonCharacter 2023-01-10 17:23:00 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -8,6 +8,17 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        //what if I just used all the ram?
+        int[] allChars = new int[(int)Character.MAX_VALUE + 1];
+        int max = 0;
+        char maxChar = ' ';
+        for(int i = 0; i<str.length() ; i++){
+            allChars[(int)str.charAt(i)] += 1;
+            if(allChars[(int)str.charAt(i)] > max){
+                max = allChars[(int)str.charAt(i)];
+                maxChar = str.charAt(i);
+            }
+        }
+        return maxChar;
     }
 }
