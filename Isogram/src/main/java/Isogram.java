8582670ed92772e174b39926d3406061b2ffb846
Keven N Mitchell <keven.mitchell@revature.com>
2023-01-10 20:35:54 +0000

    Saved progress on Isogram 2023-01-10 20:35:54 +0000

--- Isogram/src/main/java/Isogram.java
+++ Isogram/src/main/java/Isogram.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 
 public class Isogram {
     /**
@@ -13,6 +14,11 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        HashSet<Character> letters = new HashSet<>();
+        for(int i=0;i<str.length();i++){
+           if(letters.contains(str.charAt(i)))return false;
+           else letters.add(str.charAt(i));
+        }
+        return true;
     }
 }
